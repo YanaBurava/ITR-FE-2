6a814828f0fb40f98567f61b5ce6916591785955
Yanina Burova <yanina.burova@betfred.com>
2025-02-09 23:18:04 +0100

    refactoring pagination

--- shelter/js/pagination.js
+++ shelter/js/pagination.js
@@ -1,5 +1,35 @@
 import petsArray from './petsInfo.js'; 
 import { createPetCard } from './popupModal.js';
+const AMOUNT_OF_PETS = petsArray.length;
+let petsCount = 0;  
+let totalPages = 0; 
+
+const mobileWidthMediaQuery = window.matchMedia('(max-width: 767px)');
+const tabletWidthMediaQuery = window.matchMedia(
+  '(min-width: 768px) and (max-width: 1279px)'
+);
+const desktopWidthMediaQuery = window.matchMedia('(min-width: 1280px)');
+
+// Определяем количество питомцев на странице в зависимости от ширины экрана
+switch (true) {
+  case mobileWidthMediaQuery.matches:
+    petsCount = 3;
+    totalPages = Math.ceil(AMOUNT_OF_PETS / petsCount); // Округление вверх
+    break;
+  case tabletWidthMediaQuery.matches:
+    petsCount = 6;
+    totalPages = Math.ceil(AMOUNT_OF_PETS / petsCount); // Округление вверх
+    break;
+  case desktopWidthMediaQuery.matches:
+    petsCount = 8;
+    totalPages = Math.ceil(AMOUNT_OF_PETS / petsCount); // Округление вверх
+    break;
+  default:
+    break;
+}
+
+console.log(`Pets per page: ${petsCount}`);
+console.log(`Total pages: ${totalPages}`);
 
 const itemsPerPage = 3; // Количество питомцев на одной странице
 let currentPage = 1;
