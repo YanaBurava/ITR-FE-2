9e3c05e4a2e028f8f7fdaedfd21527167ca24588
Yanina Burova <yanina.burova@betfred.com>
2025-02-09 23:25:31 +0100

    refactoring

--- shelter/js/pagination.js
+++ shelter/js/pagination.js
@@ -1,8 +1,9 @@
 import petsArray from './petsInfo.js'; 
 import { createPetCard } from './popupModal.js';
+
 const AMOUNT_OF_PETS = petsArray.length;
 let petsCount = 0;  
-let totalPages = 0; 
+let totalPages = 0;
 
 const mobileWidthMediaQuery = window.matchMedia('(max-width: 767px)');
 const tabletWidthMediaQuery = window.matchMedia(
@@ -10,49 +11,45 @@ const tabletWidthMediaQuery = window.matchMedia(
 );
 const desktopWidthMediaQuery = window.matchMedia('(min-width: 1280px)');
 
-// Определяем количество питомцев на странице в зависимости от ширины экрана
 switch (true) {
   case mobileWidthMediaQuery.matches:
     petsCount = 3;
-    totalPages = Math.ceil(AMOUNT_OF_PETS / petsCount); // Округление вверх
     break;
   case tabletWidthMediaQuery.matches:
     petsCount = 6;
-    totalPages = Math.ceil(AMOUNT_OF_PETS / petsCount); // Округление вверх
     break;
   case desktopWidthMediaQuery.matches:
     petsCount = 8;
-    totalPages = Math.ceil(AMOUNT_OF_PETS / petsCount); // Округление вверх
     break;
   default:
     break;
 }
 
+totalPages = Math.ceil(AMOUNT_OF_PETS / petsCount); 
+
 console.log(`Pets per page: ${petsCount}`);
 console.log(`Total pages: ${totalPages}`);
 
-const itemsPerPage = 3; // Количество питомцев на одной странице
 let currentPage = 1;
+
 function displayPets(page) {
     const petsContainer = document.getElementById('pets-c');
-    petsContainer.innerHTML = '';
+    petsContainer.innerHTML = ''; 
   
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
+    const startIndex = (page - 1) * petsCount;
+    const endIndex = startIndex + petsCount;
     const petsToShow = petsArray.slice(startIndex, endIndex);
-  
     petsToShow.forEach(pet => {
       const petCard = createPetCard(pet);
       petsContainer.appendChild(petCard);
     });
   
     updatePagination(page);
-  }
-  
-  function updatePagination(page) {
-    const totalPages = Math.ceil(petsArray.length / itemsPerPage);
-  
-    // Обновляем текст номера страницы
+}
+
+function updatePagination(page) {
+    const totalPages = Math.ceil(petsArray.length / petsCount); 
+
     const pageNumber = document.getElementById('page-number');
     pageNumber.textContent = page;
   
@@ -76,9 +73,9 @@ function displayPets(page) {
       doubleRight.classList.remove('disabled');
       right.classList.remove('disabled');
     }
-  }
-  
-  function setupPagination() {
+}
+
+function setupPagination() {
     const doubleLeft = document.getElementById('double-left');
     const left = document.getElementById('left');
     const right = document.getElementById('right');
@@ -99,7 +96,6 @@ function displayPets(page) {
     });
   
     right.addEventListener('click', () => {
-      const totalPages = Math.ceil(petsArray.length / itemsPerPage);
       if (currentPage < totalPages) {
         currentPage++;
         displayPets(currentPage);
@@ -107,14 +103,14 @@ function displayPets(page) {
     });
   
     doubleRight.addEventListener('click', () => {
-      const totalPages = Math.ceil(petsArray.length / itemsPerPage);
       if (currentPage < totalPages) {
         currentPage = totalPages;
         displayPets(currentPage);
       }
     });
-  }
-  document.addEventListener('DOMContentLoaded', () => {
+}
+
+document.addEventListener('DOMContentLoaded', () => {
     displayPets(currentPage);  
     setupPagination();
-  });
+});
